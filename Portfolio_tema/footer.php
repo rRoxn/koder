f2ef1ade13e3c58bc545cbe9914dfecbd249618b
rRoxn <rroxen03@gmail.com>
2024-05-13 07:19:30 +0200

    Update

--- Portfolio_tema/footer.php
+++ Portfolio_tema/footer.php
@@ -1,5 +1,19 @@
     <footer>Footer
-
+    <div class="footer-section copyright">
+        <p>&copy; 2024 Robin Roxenrot</p> 
+        </div>
+        <div class="footer-section contact">
+        <p>Email: rroxen03@gmail.com</p>
+        <p>Mobil: 0702366434</p>
+         </div>
+    <div class="footer-section social-media">
+        <p>Länkar till </p>    
+		    <a href="https://www.facebook.com/rroxenrot1/" target="_blank"><i class="fa-brands fa-facebook" style="color: #63E6BE;"></i></a>
+            <a href="https://discord.gg/Db9WmPSAcE" target="_blank"><i class="fa-brands fa-discord" style="color: #63E6BE;"></i></a>
+		    <a href="https://www.instagram.com/robinroxenrot/" target="_blank"><i class="fa-brands fa-instagram" style="color: #63E6BE;"></i></a>
+            <a href="https://github.com/rRoxn" target="_blank"><i class="fa-brands fa-github" style="color: #63E6BE;"></i></a>
+            <p id="datum"> </p>
+         </div>
     </footer>
     </div>
     </body>
